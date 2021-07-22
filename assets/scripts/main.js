ed61ddaee20bd5d48276b53932d219cddc1383f5
rashad <omar_rashad@aol.com>
2021-07-22 22:39:36 +0200

    Repo: Local linters

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,83 +1,85 @@
-// All of your book objects are going to be stored in a simple array
-let myLibrary = [];
-let body = document.body;
-let myForm = document.getElementById('myForm');
-let newBookBtn = document.createElement('input');
+const myLibrary = [];
+const { body } = document;
+const newBookBtn = document.createElement('input');
+const myForm = document.getElementById('myForm');
 newBookBtn.value = 'Add new book';
 newBookBtn.type = 'button';
+
+function Book(author, title, pages, doneReading) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.doneReading = doneReading;
+}
+
+function toggleRead(index, lib) {
+  if (lib[index].doneReading === true) {
+    lib[index].doneReading = false;
+  } else {
+    lib[index].doneReading = true;
+  }
+  displayLibrary(lib);
+}
+
+function removeBook(index, lib) {
+  myLibrary.splice(index, 1);
+  displayLibrary(lib);
+}
+
+function displayLibrary(lib) {
+  newBookBtn.style.display = 'block';
+  const h1 = document.querySelector('.pageHeader');
+  h1.innerHTML = 'Your Library :';
+  const bookList = document.querySelector('ul');
+  bookList.innerHTML = '';
+
+  Object.keys(lib).forEach((bookIndex) => {
+    const bookItem = document.createElement('li');
+    const removeBtn = document.createElement('button');
+    const readToggle = document.createElement('button');
+    removeBtn.innerHTML = 'Remove Book';
+    removeBtn.onclick = () => { removeBook(bookIndex, lib); };
+    readToggle.innerHTML = 'Toggle Read';
+    readToggle.onclick = () => { toggleRead(bookIndex, lib); };
+    bookItem.innerHTML = `Title : ${lib[bookIndex].title}<br> Read : ${lib[bookIndex].doneReading}<br>`;
+    bookItem.appendChild(removeBtn);
+    bookItem.appendChild(readToggle);
+    bookList.appendChild(bookItem);
+  });
+  body.appendChild(bookList);
+}
+
+function addBookToLibrary() {
+  myForm.style.display = 'none';
+  let reading;
+  const author = document.getElementById('author').value;
+  const title = document.getElementById('title').value;
+  const pages = document.getElementById('pages').value;
+  if (document.getElementById('readingt').checked) {
+    reading = document.getElementById('readingt').value;
+  } else {
+    reading = document.getElementById('readingf').value;
+  }
+  const newBook = new Book(author, title, pages, reading);
+  myLibrary.push(newBook);
+  displayLibrary(myLibrary);
+}
+
+function displayForm() {
+  newBookBtn.style.display = 'none';
+  myForm.style.display = 'block';
+  const formBtn = document.getElementById('formBtn');
+  formBtn.onclick = addBookToLibrary;
+}
+
 newBookBtn.onclick = displayForm;
+// All of your book objects are going to be stored in a simple array
+
 body.insertBefore(newBookBtn, body.children[1]);
 myForm.style.display = 'none';
 
 // Book object constructor
-function Book(author, title, pages, doneReading) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.doneReading = doneReading;
-}
 
-// gonna use it after asking user to enter book name 
-function addBookToLibrary() {
-    myForm.style.display = 'none';
-    let author = document.getElementById('author').value;
-    let title = document.getElementById('title').value;
-    let pages = document.getElementById('pages').value;
-    if (document.getElementById('readingt').checked) {
-        var reading = document.getElementById('readingt').value;
-      }    else {
-        var reading = document.getElementById('readingf').value;
-      } 
-    let newBook = new Book(author, title, pages, reading)
-    console.log(newBook);
-    myLibrary.push(newBook);
-    displayLibrary(myLibrary);
-}
-
+// gonna use it after asking user to enter book name
 
 // function that loops through the array and displays each book on the page.
-function displayLibrary(lib) {
-    newBookBtn.style.display = 'block';
-    let h1 = document.querySelector('.pageHeader');
-    h1.innerHTML = 'Your Library :';
-    let bookList = document.querySelector('ul');
-    bookList.innerHTML = '';
-    
-    for (let bookIndex in lib) {
-        let bookItem = document.createElement('li');
-        let removeBtn = document.createElement('button');
-        let readToggle = document.createElement('button');
-        removeBtn.innerHTML = 'Remove Book';
-        removeBtn.onclick = () => {removeBook(bookIndex, lib)};
-        readToggle.innerHTML = 'Toggle Read';
-        readToggle.onclick = () => {toggleRead(bookIndex, lib)};
-        bookItem.innerHTML = 'Title : ' + lib[bookIndex].title + '<br> Read : ' + lib[bookIndex].doneReading + '<br>';
-        bookItem.appendChild(removeBtn);
-        bookItem.appendChild(readToggle);
-        bookList.appendChild(bookItem);
-    }
-
-    body.appendChild(bookList);
-}
-
-
-function displayForm() {
-    newBookBtn.style.display = 'none';
-    myForm.style.display = 'block';
-    let formBtn = document.getElementById('formBtn');
-    formBtn.onclick = addBookToLibrary;
-}
-
-function removeBook(index, lib) {
-    myLibrary.splice(index,1);
-    displayLibrary(lib);
-}
-
-function toggleRead(index,lib) {
-    if (lib[index].doneReading === true) {
-        lib[index].doneReading = false
-    } else {
-        lib[index].doneReading = true
-    }
-    displayLibrary(lib);
-}

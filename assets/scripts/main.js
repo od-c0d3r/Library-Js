fc8ab830865c2cb4c683dc480343926db9043e13
od-c0d3r <omar_rashad@aol.com>
2021-07-22 10:58:56 +0200

    Feat: Take User input for New Book

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,33 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+// All of your book objects are going to be stored in a simple array
+let myLibrary = [];
+body = document.querySelector('.body')
+
+// Book object constructor
+function Book(name) {
+    this.name = name
+}
+
+// gonna use it after asking user to enter book name 
+function addBookToLibrary() {
+    let name = window.prompt("Please Enter Book name: ");
+    let newBook = new Book(name)
+    myLibrary.push(newBook);
+    displayLibrary(myLibrary)
+}
+
+
+// function that loops through the array and displays each book on the page.
+function displayLibrary(lib) {
+    let h1 = document.querySelector('.pageHeader');
+    h1.innerHTML = 'Your Library :';
+    let bookList = document.querySelector('ul');
+    bookList.innerHTML = ''
+    
+    for (let book in lib) {
+        let bookItem = document.createElement('li');
+        bookItem.innerHTML = lib[book].name;
+        bookList.appendChild(bookItem);
+    }
+
+    body.appendChild(bookList);
+}
